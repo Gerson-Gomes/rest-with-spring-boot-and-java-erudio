6ea16754c6eaecc0baad38e452a783d7df9102da
Gerson-Gomes <gersoncastrogomes95@gmail.com>
2024-06-25 00:47:27 -0300

    update

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Controller/PersonController.java
@@ -0,0 +1,30 @@
+package br.com.erudio.Controller;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private PersonServices services;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable (value = "id") String id){
+        return services.findById(id);
+    }
+
+
+}
